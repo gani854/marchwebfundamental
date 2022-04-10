7e91171229ce622b156a59ae437976c70f39bfe2
gani854 <100420408+gani854@users.noreply.github.com>
2022-04-10 17:55:29 +0530

    Add files via upload

---
+++ swap.js
@@ -0,0 +1,10 @@
+let a = prompt("Enter  the first variable");
+let b = prompt("Enter  the first variable");
+let temp;
+temp=a;
+a=b;
+b=temp;
+
+console.log("the value of a after swapping: ${a}");
+console.log("the value of b after swappig: ${b}");
+
